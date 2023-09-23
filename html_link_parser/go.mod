4352e20240364d73fa1fa02a95816f509adfde83
cihancanayyildiz <cihancanayyildiz@gmail.com>
2023-09-23 17:26:41 +0300

    html link(<a>) parser

---
+++ html_link_parser/go.mod
@@ -0,0 +1,5 @@
+module html_link_parser
+
+go 1.21.1
+
+require golang.org/x/net v0.15.0 // indirect
---
+++ html_link_parser/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}	
+
+func main() {
+	filepath := flag.String("file", "ex1.html", "html file path")
+	flag.Parse()
+	file, err := os.ReadFile(*filepath)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tkn := html.NewTokenizer(strings.NewReader(string(file)))
+
+	var link []Link
+
+	var tempLink = Link{
+		Href: "",
+		Text: "",
+	}
+
+	var linkFlag bool = false
+	var htmlEndFlag bool = false
+	for {
+		tt := tkn.Next()
+		switch {
+			case tt == html.ErrorToken:
+				htmlEndFlag = true
+
+			case tt == html.StartTagToken:
+				t := tkn.Token()
+				if t.Data == "a" {
+					tempLink.Href = ""
+					tempLink.Text= ""
+					linkFlag = true
+					for i := range t.Attr {
+						if(t.Attr[i].Key == "href"){
+							tempLink.Href = t.Attr[i].Val
+							break
+						}
+					}
+				}
+
+        	case tt == html.TextToken:
+				t := tkn.Token()
+				if linkFlag {
+					t.Data = strings.TrimSpace(t.Data)
+					tempLink.Text += t.Data
+				}
+
+			case tt == html.EndTagToken:
+				t := tkn.Token()
+				if t.Data == "a" {
+					linkFlag = false
+					link = append(link, tempLink)
+				}	
+		}
+		if htmlEndFlag {
+			break
+		}
+	}
+	fmt.Println(link)
+}
